f8ed36547f176e9727f2d343d51e2f13817f2613
Vatsal Joshi <vatsalasitkumar.joshi@mavs.uta.edu>
2024-07-28 21:08:13 -0700

    Make Anymate look similar to other plugins

--- plugin/anymate/anymate.js
+++ plugin/anymate/anymate.js
@@ -1,33 +1,35 @@
-// anymate.js
-var cmuSerifRegular;
-fetch('../../dist/theme/fonts/computer-modern/cmu.serif-regular.ttf').then(res => res.arrayBuffer()).then(data => cmuSerifRegular = opentype.parse(data));
+// Anymate Plugin
+// Allows any element to react to reveal.js events.
+// Written By: Vatsal Joshi
+// Email: vajoshi.baroda@gmail.com
 
-class anymate {
-    init(reveal) {
-        reveal.on('ready', this.handleSlidesReady.bind(this));
-        reveal.on('slidechanged', this.handleSlideChange.bind(this));
-        reveal.on('fragmentshown', this.handleFragShown.bind(this));
-        reveal.on('fragmenthidden', this.handleFragHidden.bind(this));
-        reveal.addKeyBinding({ keyCode: 32, key: 'space', descriptio: 'Play or Pause the animations' }, this.playPauseAnim.bind(this));
-        reveal.addKeyBinding({ keyCode: 82, key: 'R', descriptio: 'Reset the animations' }, this.callResetFuns.bind(this));
-        reveal.addKeyBinding({ keyCode: 188, key: '<', descriptio: 'Slow down the animations' }, this.decreaseAnimUpdateDt.bind(this));
-        reveal.addKeyBinding({ keyCode: 190, key: '>', descriptio: 'Speed up the animations' }, this.increaseAnimUpdateDt.bind(this));
-    }
+// Following four line just works.
+// Copied from one of the reveal.js plugins.
+// The idea here is to create a function expression.
+// The advantage is that functions and variables defined inside this function are not accessible by the outside world unless attached as the members of the object being returned by the function. This basically behaves like a single object with hidden members.
+!function(e, t) {
+    "object" == typeof exports && "undefined" != typeof module ? module.exports = t() : "function" == typeof define && define.amd ? define(t) : (e = "undefined" != typeof globalThis ? globalThis : e || self).Anymate = t()
+}(this, (function() {
+    "use strict";
 
-    #animPlaying = false;
-    #animObjs = new Array();
-    #animUpdateIntIdx = null;
-    #animUpdateCounter = 0;
-    #animUpdateDt = 1;      // This exists for the update function that depend on ode solvers. It can be used to change the integration time step on the fly.
+    // The reveal.js instance this plugin is attached to
+    let slidesDeck;
+
+    // Some necessary variables to keep track of the system state.
+    let animPlaying = false;
+    let animObjs = new Array();
+    let animUpdateIntIdx = null;
+    let animUpdateCounter = 0;
+    let animUpdateDt = 1;      // This exists for the update function that depend on ode solvers. It can be used to change the integration time step on the fly.
 
     // Functions for animations to register themselves so that update and pause can be controlled
-    registerAnim(animObj) {
-        this.#animObjs.push(animObj);
+    function registerAnim(animObj) {
+        animObjs.push(animObj);
     }
 
     // Function to find indices to the elements that are on the current slide
-    #setCurrentSlideElems(idxh, idxv) {
-        for (let animObj of this.#animObjs)
+    function setCurrentSlideElems(idxh, idxv) {
+        for (let animObj of animObjs)
             if (idxh == animObj.idxh && idxv == animObj.idxv)
                 animObj.onCurrentSlide = true;
             else
@@ -35,74 +37,74 @@ class anymate {
     }
 
     // Function to call the update functions of the animations that are on the current slide
-    #callUpdateFuns() {
-        this.#animUpdateCounter += this.#animUpdateDt;
-        for (let animObj of this.#animObjs)
+    function callUpdateFuns() {
+        animUpdateCounter += animUpdateDt;
+        for (let animObj of animObjs)
             if (animObj.onCurrentSlide && (typeof animObj.update === "function"))
-                animObj.update(this.#animUpdateCounter, this.#animUpdateDt, animObj.onCurrentFragm);
+                animObj.update(animUpdateCounter, animUpdateDt, animObj.onCurrentFragm);
     }
 
     // Function to call the play functions of the animations that are on the current slide
-    #callPlayFuns() {
-        for (let animObj of this.#animObjs)
+    function callPlayFuns() {
+        for (let animObj of animObjs)
             if (animObj.onCurrentSlide && (typeof animObj.play === "function"))
                 animObj.play();
     }
 
     // Function to call the pause functions of the animations that are on the current slide
-    #callPauseFuns() {
-        for (let animObj of this.#animObjs)
+    function callPauseFuns() {
+        for (let animObj of animObjs)
             if (animObj.onCurrentSlide && (typeof animObj.pause === "function"))
                 animObj.pause();
     }
 
     // Function to call the reset functions of the animations that are on the current slide
-    callResetFuns() {
-        if (this.#animPlaying) {
-            clearInterval(this.#animUpdateIntIdx);
-            this.#animPlaying = false;
-            this.#callPauseFuns();
+    function callResetFuns() {
+        if (animPlaying) {
+            clearInterval(animUpdateIntIdx);
+            animPlaying = false;
+            callPauseFuns();
         }
-        this.#animUpdateCounter = 0;
-        this.#animUpdateDt = 1;
-        for (let animObj of this.#animObjs)
+        animUpdateCounter = 0;
+        animUpdateDt = 1;
+        for (let animObj of animObjs)
             if (animObj.onCurrentSlide && (typeof animObj.reset === "function"))
                 animObj.reset();
     }
 
     // Function to update and pause animation on the respective slied
-    playPauseAnim() {
-        if (this.#animPlaying) {
-            clearInterval(this.#animUpdateIntIdx);
-            this.#animPlaying = false;
-            this.#callPauseFuns();
+    function playPauseAnim() {
+        if (animPlaying) {
+            clearInterval(animUpdateIntIdx);
+            animPlaying = false;
+            callPauseFuns();
         }
         else {
-            this.#callPlayFuns();
-            this.#animPlaying = true;
-            this.#animUpdateIntIdx = setInterval(this.#callUpdateFuns.bind(this), 16);
+            callPlayFuns();
+            animPlaying = true;
+            animUpdateIntIdx = setInterval(callUpdateFuns.bind(this), 16);
         }
     }
 
     // Function to handle the slide change event
-    handleSlideChange(event) {
-        this.callResetFuns();
-        this.#setCurrentSlideElems(event.indexh, event.indexv);
+    function handleSlideChange(event) {
+        callResetFuns();
+        setCurrentSlideElems(event.indexh, event.indexv);
     }
 
     // Function to handle the slides ready event
-    handleSlidesReady(event) {
-        if (this.#animObjs.length)
-            this.#setCurrentSlideElems(event.indexh, event.indexv);
+    function handleSlidesReady(event) {
+        if (animObjs.length)
+            setCurrentSlideElems(event.indexh, event.indexv);
         else
-            setTimeout(this.handleSlidesReady.bind(this, event), 500);
+            setTimeout(handleSlidesReady.bind(this, event), 500);
     }
 
     // Function to handle the fragment shown event
-    handleFragShown(event) {
-        if (this.#animObjs.length) {
+    function handleFragShown(event) {
+        if (animObjs.length) {
             for (let frag of event.fragments)
-                for (let animObj of this.#animObjs)
+                for (let animObj of animObjs)
                     if (frag.id == animObj.id) {
                         animObj.onCurrentFragm = true;
                         if (typeof animObj.fragShown === "function")
@@ -110,13 +112,13 @@ class anymate {
                     }
         }
         else
-            setTimeout(this.handleFragShown.bind(this, event), 500);
+            setTimeout(handleFragShown.bind(this, event), 500);
     }
 
     // Function to handle the fragment hidden event
-    handleFragHidden(event) {
+    function handleFragHidden(event) {
         for (let frag of event.fragments)
-            for (let animObj of this.#animObjs)
+            for (let animObj of animObjs)
                 if (frag.id == animObj.id) {
                     animObj.onCurrentFragm = false;
                     if (typeof animObj.fragHidden === "function")
@@ -125,58 +127,31 @@ class anymate {
     }
 
     // Change global time speed
-    increaseAnimUpdateDt() {
-        this.#animUpdateDt += 0.1;
+    function increaseAnimUpdateDt() {
+        animUpdateDt += 0.1;
     }
-    decreaseAnimUpdateDt() {
-        this.#animUpdateDt -= 0.1;
-        if (this.#animUpdateDt < 0) {
-            this.#animUpdateDt = 0;
+    function decreaseAnimUpdateDt() {
+        animUpdateDt -= 0.1;
+        if (animUpdateDt < 0) {
+            animUpdateDt = 0;
         }
     }
-}
 
-class anim {
-    #initAnimIntIdx = null;
-    constructor(elemID) {
-        this.id = elemID;
-        this.#initAnimIntIdx = setInterval(this.#initializeAnimation.bind(this), 100);
-    }
+    return {
+		id: 'anymate',
 
-    // Function for the animation to initialize itself
-    #initializeAnimation() {
-        if (document.readyState === 'complete') {
-            this.elem = document.getElementById(this.id);
-            if (this.elem != null) {
-                let slide = this.elem.closest('section');
-                if (slide != null) {
-                    this.idxh = Reveal.getIndices(slide).h;
-                    this.idxv = Reveal.getIndices(slide).v;
-                    this.onCurrentSlide = false;
-                    this.onCurrentFragm = false;
-                    clearInterval(this.#initAnimIntIdx);
-                    if (typeof this.init === "function")
-                        this.init();
-                    anymateControl.registerAnim(this);
-                }
-                else {
-                    clearInterval(this.#initAnimIntIdx);
-                    throw this.id + ": This element is not inside a 'section' element.";
-                }
-            }
-            else {
-                clearInterval(this.#initAnimIntIdx);
-                throw this.id + ": This element doesn't exist.";
-            }
-        }
-    }
-}
+		init(reveal) {
+            slidesDeck = reveal;
+            slidesDeck.on('ready', handleSlidesReady);
+            slidesDeck.on('slidechanged', handleSlideChange);
+            slidesDeck.on('fragmentshown', handleFragShown);
+            slidesDeck.on('fragmenthidden', handleFragHidden);
+            slidesDeck.addKeyBinding({ keyCode: 32, key: 'space', descriptio: 'Play or Pause the animations' }, playPauseAnim);
+            slidesDeck.addKeyBinding({ keyCode: 82, key: 'R', descriptio: 'Reset the animations' }, callResetFuns);
+            slidesDeck.addKeyBinding({ keyCode: 188, key: '<', descriptio: 'Slow down the animations' }, decreaseAnimUpdateDt);
+            slidesDeck.addKeyBinding({ keyCode: 190, key: '>', descriptio: 'Speed up the animations' }, increaseAnimUpdateDt);
+        },
 
-const anymateControl = new anymate();
-
-window.Anymate = window.Anymate || {
-    id: 'Anymate',
-    init: function (reveal) {
-        anymateControl.init(reveal);
-    }
-};
+        register: registerAnim
+	}
+}));
